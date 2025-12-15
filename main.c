d49e1d3ded1f6180aa5c7742e89ac5840b9e57fd
pixzrr <91895158+pixzrr@users.noreply.github.com>
2025-12-15 15:35:55 +0100

    v12
    correction colisions_enemis + fin_jeu

--- main.c
+++ main.c
@@ -107,6 +107,26 @@ int main()
             i=0;
             personnage_principal[INDEX_PERSONNAGE_VITALITE]--;
         }
+
+        //Detecter fin du jeu
+        if (personnage_principal[INDEX_PERSONNAGE_NBRE_VIES_RESTANTES] == 0) {
+            char texte[] = "Perdu !";
+            for (int i=0 ; i<strlen(texte) ; i++) {
+                init_text_cursor(TAILLE_SCENE_X/2+i, TAILLE_SCENE_Y/2, RED,BLACK);
+                printf("%c", texte[i]);
+            }
+            init_text_cursor(0, TAILLE_SCENE_Y, WHITE,BLACK);
+            return 0;
+        }
+        if (personnage_principal[INDEX_PERSONNAGE_NBRE_PIECES_RECOLTEES] == 5) {
+            char texte[] = "Gagné !";
+            for (int i=0 ; i<strlen(texte) ; i++) {
+                init_text_cursor(TAILLE_SCENE_X/2+i, TAILLE_SCENE_Y/2, RED,BLACK);
+                printf("%c", texte[i]);
+            }
+            init_text_cursor(0, TAILLE_SCENE_Y, WHITE,BLACK);
+            return 0;
+        }
     }
 
     return 0;
@@ -362,9 +382,13 @@ int detecter_collision(unsigned int personnage_principal[NBRE_PROPRIETES_PERSONN
     int collision = 0;
 
         if (personnage_principal[0] == ennemi[0] && personnage_principal[1] == ennemi[1]){
-                Sleep(1500);
                 collision = 1;
-
+                char message[] = "Colisionnage";
+                init_text_cursor(0, TAILLE_SCENE_Y, RED, BLACK);
+                puts(message);
+                sleep(1,5);
+                init_text_cursor(0, TAILLE_SCENE_Y, BLACK, BLACK);
+                puts(message);
         }
 
     return collision;
